2231483e2d78cab6d8a972cefca7d841340985e8
David Garvie <garvie88@gmail.com>
2020-08-13 21:22:59 +0100

    Create project and simple express server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => res.send('hello'));
+
+app.listen('8080', () => {
+  console.log('App running on 8080');
+})
