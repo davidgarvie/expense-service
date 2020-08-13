5b2a5b053c3404007f68ca900ac5d09ba8d706a9
David Garvie <garvie88@gmail.com>
2020-08-13 22:11:48 +0100

    Update server to log requests with pino

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const pino = require('pino-http')
+const { logger } = require('./src/helpers')
 const app = express();
+
+app.use(pino({
+  logger,
+}));
 
 app.use("/", (req, res) => res.send("hello"));
 
 app.listen("8080", () => {
-  console.log("App running on 8080");
+  logger.info("App running on 8080");
 });
