25317ef63340aef23af9f69db78bd4c550f7a19b
David Garvie <garvie88@gmail.com>
2020-08-15 21:28:47 +0100

    feat: update route to create a new expense

--- src/router/expense.js
+++ src/router/expense.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { getExpenses } = require("../controllers");
+const { createExpense, getExpenses } = require("../controllers");
 const { controllerHelper } = require("../helpers");
 
 const router = new Router();
 
 router.get("/", controllerHelper(getExpenses));
+router.post("/", controllerHelper(createExpense));
 
 exports.expenseRouter = router;
