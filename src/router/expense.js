95bd819a8e9fcb561248f07ddc4e87c45590b4d0
David Garvie <garvie88@gmail.com>
2020-08-16 09:31:19 +0100

    feat: update expense router to allow user to update an expense

--- src/router/expense.js
+++ src/router/expense.js
@@ -4,6 +4,7 @@ const {
   deleteExpense,
   getExpense,
   getExpenses,
+  updateExpense,
 } = require("../controllers");
 const { controllerHelper, paramHelper } = require("../helpers");
 
@@ -15,5 +16,6 @@ router.get("/", controllerHelper(getExpenses));
 router.post("/", controllerHelper(createExpense));
 router.get("/:id", (req, res) => res.send(req.locals.model));
 router.delete("/:id", controllerHelper(deleteExpense));
+router.put("/:id", controllerHelper(updateExpense));
 
 exports.expenseRouter = router;
