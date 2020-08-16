7a2ba52acdd6f27944f6142ae246a1564789d0e7
David Garvie <garvie88@gmail.com>
2020-08-16 08:47:19 +0100

    feat: update router to allow a user to delete an expense

--- src/router/expense.js
+++ src/router/expense.js
@@ -1,5 +1,10 @@
 const { Router } = require("express");
-const { createExpense, getExpense, getExpenses } = require("../controllers");
+const {
+  createExpense,
+  deleteExpense,
+  getExpense,
+  getExpenses,
+} = require("../controllers");
 const { controllerHelper, paramHelper } = require("../helpers");
 
 const router = new Router();
@@ -8,8 +13,7 @@ router.param(":id", paramHelper(getExpense));
 
 router.get("/", controllerHelper(getExpenses));
 router.post("/", controllerHelper(createExpense));
-router.get("/:id", (req, res) => {
-  res.send(req.locals.model);
-});
+router.get("/:id", (req, res) => res.send(req.locals.model));
+router.delete("/:id", controllerHelper(deleteExpense));
 
 exports.expenseRouter = router;
