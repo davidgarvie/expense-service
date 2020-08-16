92b3cd59ca706b5604ef752d90041fa6b4e55382
David Garvie <garvie88@gmail.com>
2020-08-16 08:28:22 +0100

    feat: allow a user to get a specific expense

--- src/router/expense.js
+++ src/router/expense.js
@@ -1,10 +1,15 @@
 const { Router } = require("express");
-const { createExpense, getExpenses } = require("../controllers");
-const { controllerHelper } = require("../helpers");
+const { createExpense, getExpense, getExpenses } = require("../controllers");
+const { controllerHelper, paramHelper } = require("../helpers");
 
 const router = new Router();
 
+router.param(":id", paramHelper(getExpense));
+
 router.get("/", controllerHelper(getExpenses));
 router.post("/", controllerHelper(createExpense));
+router.get("/:id", (req, res) => {
+  res.send(req.locals.model);
+});
 
 exports.expenseRouter = router;
